7dfa3b269859a9c31461c8c6e50ef6ce263bd8c7
Joao Maria <joaopgmaria@gmail.com>
2019-12-04 15:15:19 +0000

    Add day 1 part 2

--- lib/Day1.ex
+++ lib/Day1.ex
@@ -9,6 +9,12 @@ defmodule Day1 do
       |> Enum.reduce(0, fn mass, acc -> acc + get_fuel(mass) end)
   end
 
+  def part2() do
+    get_input()
+      |> get_masses()
+      |> Enum.reduce(0, fn mass, acc -> acc + get_fuel_corrected(mass) end)
+  end
+
   defp get_masses(input) do
     input
       |> String.replace("\r", "")
@@ -17,4 +23,11 @@ defmodule Day1 do
   end
 
   def get_fuel(mass), do: Kernel.trunc(mass/3)-2
+
+  def get_fuel_corrected(mass, total \\ 0)
+  def get_fuel_corrected(mass, total) when mass <= 8, do: total
+  def get_fuel_corrected(mass, total) do
+    fuel = get_fuel(mass)
+    get_fuel_corrected(fuel, total+fuel)
+  end
 end
--- test/Day1_test.exs
+++ test/Day1_test.exs
@@ -3,10 +3,17 @@ defmodule DTest do
   doctest Day1
 
   @tag day: 1
-  test "greets the world" do
+  test "gets fuel" do
     assert Day1.get_fuel(12) == 2
     assert Day1.get_fuel(14) == 2
     assert Day1.get_fuel(1969) == 654
     assert Day1.get_fuel(100756) == 33583
+  end
+
+  @tag day: 1
+  test "gets (good) fuel" do
+    assert Day1.get_fuel_corrected(14) == 2
+    assert Day1.get_fuel_corrected(1969) == 966
+    assert Day1.get_fuel_corrected(100756) == 50346
   end
 end
